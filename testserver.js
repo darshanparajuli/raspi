16cfdefcd088e69fd3e4cb9380144d070461a225
Darshan Parajuli <parajulidarshan@gmail.com>
2014-06-13 20:06:02 -0700

    update

--- testserver.js
+++ testserver.js
@@ -1,5 +1,8 @@
 var net = require('net');
 var http = require('http');
+var gpio = require('rpi-gpio');
+
+var temp = false;
 
 var server = net.createServer(function(c) {
   console.log('new client!');
@@ -11,6 +14,13 @@ var server = net.createServer(function(c) {
   c.setEncoding('utf8');
   
   c.on('data', function(d) {
+    if (temp)
+      temp = false;
+    else
+      temp = true;
+
+    write();
+    
     var data = JSON.parse(d);
     console.log(data.name + ": " + data.message);
     c.write(JSON.stringify({"name": "server", "message": "hello client"}));
@@ -20,3 +30,16 @@ var server = net.createServer(function(c) {
 server.listen(8080, function() {
   console.log('server bound');
 });
+
+gpio.on('change', function(channel, value) {
+  console.log('Channel ' + channel + ' value is now ' + value);
+});
+
+gpio.setup(16, gpio.DIR_OUT);
+
+function write() {
+  gpio.write(16, temp, function(err) {
+    if (err) throw err;
+    console.log("Written to pin!");
+  });
+}
